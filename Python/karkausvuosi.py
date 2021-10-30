e5f93f29326d9fb3d45174e9de36c593a0c178a1
M3920 <m3920@student.jamk.fi>
2021-10-30 15:59:28 +0300

    tehtävä karkausvuoden laskemisesta tehty

---
+++ Python/karkausvuosi.py
@@ -0,0 +1,12 @@
+# kysyy käyttäjältä syötettä ja kertoo onko vuosi karkausvuosi
+vuosi = int(input("Anna vuosi: "))
+if vuosi % 4 == 0:
+    if vuosi % 100 == 0:
+        if vuosi % 400 == 0:
+            print("Vuosi on karkausvuosi.")
+        else:
+            print("Vuosi ei ole karkausvuosi.")
+    else:
+        print("Vuosi on karkausvuosi")
+else:
+    print("Vuosi ei ole karkausvuosi.")
